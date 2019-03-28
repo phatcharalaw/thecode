f2f56f9bcac38f41e1315e8e9c7f49f796d4102f
phatcharalaw <46377227+phatcharalaw@users.noreply.github.com>
2019-03-28 17:27:14 +0700

    Add files via upload
    first

---
+++ src/main/java/com/pch/allianz/ws/demowsjaxb/DemoWsJaxbApplication.java
@@ -0,0 +1,14 @@
+package com.pch.allianz.ws.demowsjaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWsJaxbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWsJaxbApplication.class, args);
+	}
+
+}
+
